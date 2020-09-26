0f00fdd437af3f0f0a79b9084c2bc4486ea380c3
wilsonam <amy.wilson@microfocus.com>
2020-09-25 18:58:08 -0700

    Update datetimes

--- ScheduleIterator/src/Main/ScheduleDataProvider.java
+++ ScheduleIterator/src/Main/ScheduleDataProvider.java
@@ -1,24 +1,26 @@
 package Main;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class ScheduleDataProvider {
 	public static Schedule[] getSportsSchedules() {
 		return new Schedule[] {
-			new Schedule("Baseball", LocalDateTime.now(), LocalDateTime.now()),
-			new Schedule("Football", LocalDateTime.now(), LocalDateTime.now()),
-			new Schedule("Tennis", LocalDateTime.now(), LocalDateTime.now()),
-			new Schedule("Gymnastic", LocalDateTime.now(), LocalDateTime.now()),
-			new Schedule("Volleyball", LocalDateTime.now(), LocalDateTime.now()),
+			new Schedule("Baseball", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now()),
+			new Schedule("Football", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now()),
+			new Schedule("Tennis", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now()),
+			new Schedule("Gymnastic", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now()),
+			new Schedule("Volleyball", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now())
 		};
 	}
 	
 	public static Schedule[] getMusicalSchedules() {
 		return new Schedule[] {
-			new Schedule("Piano", LocalDateTime.now(), LocalDateTime.now()),
-			new Schedule("Drums", LocalDateTime.now(), LocalDateTime.now()),
-			new Schedule("Cello", LocalDateTime.now(), LocalDateTime.now()),
-			new Schedule("Viola", LocalDateTime.now(), LocalDateTime.now()),
-			new Schedule("Flute", LocalDateTime.now(), LocalDateTime.now()),
+			new Schedule("Piano", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now()),
+			new Schedule("Drums", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now()),
+			new Schedule("Cello", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now()),
+			new Schedule("Viola", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now()),
+			new Schedule("Flute", LocalDateTime.of(LocalDate.of(1985, 4, 21), LocalTime.now()), LocalDateTime.now())
 		};
 	}
 }
